61396846dd25172781649bd6e891660ef962b491
aa096 <126397865+aa096@users.noreply.github.com>
2024-11-21 14:59:29 +0100

    API-url added

---
+++ src/API/constants.jsx
@@ -0,0 +1,5 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const HOLIDAZE_URL = "/holidaze";
+export const BOOKINGS_URL = `${API_BASE_URL}/${HOLIDAZE_URL}/bookings`;
+export const PROFILES_URL = `${API_BASE_URL}/${HOLIDAZE_URL}/profiles`;
--- src/components/templates/topBanner.jsx
+++ src/components/templates/topBanner.jsx
@@ -7,7 +7,7 @@ export default function TopBanner() {
   return (
     <div className="flex flex-wrap">
       {/* Left Section */}
-      <div className="flex flex-col mx-auto w-[325px] sm:w-[420px] md:w-[500px] bg-tertiary py-7 px-7 rounded-[25px] order-2 lg:order500-1  z-10 -mt-16 sm:-mt-32 lg:mt-44 lg:ms-10">
+      <div className="flex flex-col mx-auto w-[325px] sm:w-[420px] md:w-[500px] bg-tertiary py-7 px-7 rounded-[25px] order-2 lg:order500-1  z-10 -mt-16 sm:-mt-32 lg:mt-44 lg:ms-8 xl:ml-28">
         <img src={plane} alt="plane icon flying to destination" className="w-1/2 mx-auto -mr-2 sm:mr-14" />
         <h1 className="text-[25px] font-anton uppercase -mt-11">Book Your Stay</h1>
         <p className="mt-2">Discover the perfect getaway with Holidaze</p>
@@ -26,7 +26,7 @@ export default function TopBanner() {
       </div>
 
       {/* Right Section */}
-      <div className="flex justify-center lg:justify-end lg:ms-32 my-10 mx-5 order-1 lg:order-2 w-full lg:-mt-80 z-0">
+      <div className="flex justify-center lg:justify-end lg:ms-20 xl:ml-5 my-10 mx-5 order-1 lg:order-2 w-full lg:-mt-80 z-0">
         <img
           src={topImage}
           alt="tropical holiday home next to blue sea and palms"
