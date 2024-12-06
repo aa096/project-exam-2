b85ecba623aa6318102bb69f788c60a54ee51266
aa096 <126397865+aa096@users.noreply.github.com>
2024-12-06 13:36:49 +0100

    fixed?

---
+++ src/components/templates/VenueInfo.jsx
@@ -0,0 +1,68 @@
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot, faHouseUser, faStar, faBan } from "@fortawesome/free-solid-svg-icons";
+
+const VenueInfo = ({ venue, city, country, imageUrl, imageAlt, metaInfo }) => (
+  <div className="flex flex-col justify-center items-center md:flex-row px-6 gap-7">
+    <img
+      src={imageUrl}
+      alt={imageAlt}
+      className="md:w-[400px] md:h-[500px] rounded-[20px] lg:-mt-14 lg:-ms-10 -mt-14 mx-auto object-cover object-center"
+    />
+    <div className="lg:max-w-lg xl:max-w-xl">
+      <div className="flex gap-3 uppercase">
+        <FontAwesomeIcon icon={faLocationDot} className="mt-1 text-lg" /> {city}, {country}
+      </div>
+      <p className="break-words line-clamp-8 mt-5">{venue.description}</p>
+      <div className="flex gap-3 mt-3">
+        <FontAwesomeIcon icon={faHouseUser} className="mt-1" />
+        {venue.maxGuests} Guests
+        <FontAwesomeIcon icon={faStar} className="mt-1" /> {venue.rating}/5
+      </div>
+      <div>
+        <h2 className="font-montserrat uppercase font-semibold text-[20px] my-4">$ {venue.price} per night</h2>
+        <hr className="border-[#A39C96] my-2"></hr>
+      </div>
+
+      <h3 className="uppercase text-lg font-medium mt-5">Facilities & Rules</h3>
+      <ul className="grid grid-cols-2 gap-4 mt-4">
+        {metaInfo.map((item, index) => (
+          <li
+            key={index}
+            className="relative flex items-center gap-4 text-lg"
+            aria-label={`${item.label}: ${item.value ? "Available" : "Not Available"}`}>
+            <div className="relative w-8 h-8">
+              <FontAwesomeIcon icon={item.icon} className={`text-2xl ${item.value ? "" : ""}`} />
+              {!item.value && (
+                <FontAwesomeIcon icon={faBan} className="absolute text-[#F3676A] -inset-1 text-[33px] opacity-70" />
+              )}
+            </div>
+            <span>{item.label}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+);
+
+VenueInfo.propTypes = {
+  venue: PropTypes.shape({
+    description: PropTypes.string.isRequired,
+    maxGuests: PropTypes.number.isRequired,
+    rating: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
+  city: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  imageAlt: PropTypes.string.isRequired,
+  metaInfo: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      value: PropTypes.bool.isRequired,
+      icon: PropTypes.object.isRequired,
+    })
+  ).isRequired,
+};
+
+export default VenueInfo;
--- src/components/templates/VenuePage.jsx
+++ src/components/templates/VenuePage.jsx
@@ -2,18 +2,9 @@ import { useParams } from "react-router-dom";
 import useVenue from "../../hooks/useVenue";
 import { getRandomLocation } from "./dummyLocation";
 import BookingCalendar from "../UI/BookingCalender";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faLocationDot,
-  faHouseUser,
-  faStar,
-  faWifi,
-  faUtensils,
-  faDog,
-  faSquareParking,
-  faBan,
-} from "@fortawesome/free-solid-svg-icons";
 import plane from "/assets/fly.png";
+import VenueInfo from "./VenueInfo";
+import { faWifi, faDog, faUtensils, faSquareParking } from "@fortawesome/free-solid-svg-icons";
 
 const VenuePage = () => {
   const { id } = useParams();
@@ -24,8 +15,7 @@ const VenuePage = () => {
 
   if (!venue) return <div>Venue details not found.</div>;
 
-  const { name, description, location, media, maxGuests, price, rating, meta, owner } = venue;
-
+  const { name, location, media, meta, owner } = venue;
   const city = location?.city || getRandomLocation();
   const country = location?.country || getRandomLocation();
 
@@ -43,53 +33,15 @@ const VenuePage = () => {
   return (
     <div>
       <h1 className="text-4xl font-medium text-center uppercase mt-6 mb-16">{name}</h1>
-      <div className="container py-8 bg-white rounded-[20px] shadow-custom-card">
-        <div className="flex flex-col justify-center items-center md:flex-row px-6 gap-7">
-          <img
-            src={imageUrl}
-            alt={imageAlt}
-            className="md:w-[400px] md:h-[500px] rounded-[20px] lg:-mt-14 lg:-ms-10 -mt-14 mx-auto object-cover object-center"
-          />
-          <div className="lg:max-w-lg xl:max-w-xl">
-            <div className="flex gap-3 uppercase">
-              <FontAwesomeIcon icon={faLocationDot} className="mt-1 text-lg" /> {city}, {country}
-            </div>
-            <p className="break-words line-clamp-8 mt-5">{description}</p>
-            <div className="flex gap-3 mt-3">
-              <FontAwesomeIcon icon={faHouseUser} className="mt-1" />
-              {maxGuests} Guests
-              <FontAwesomeIcon icon={faStar} className="mt-1" /> {rating}/5
-            </div>
-            <div>
-              <h2 className="font-montserrat uppercase font-semibold text-[20px] my-4">$ {price} per night</h2>
-              <hr className="border-[#A39C96] my-2"></hr>
-            </div>
-            <div>
-              <h3 className="uppercase text-lg font-medium">Facilities & Rules</h3>
-              <div>
-                <ul className="grid grid-cols-2 gap-4 mt-4">
-                  {metaInfo.map((item, index) => (
-                    <li
-                      key={index}
-                      className="relative flex items-center gap-4 text-lg"
-                      aria-label={`${item.label}: ${item.value ? "Available" : "Not Available"}`}>
-                      <div className="relative w-8 h-8">
-                        <FontAwesomeIcon icon={item.icon} className={`text-2xl ${item.value ? "" : ""}`} />
-                        {!item.value && (
-                          <FontAwesomeIcon
-                            icon={faBan}
-                            className="absolute text-[#F3676A] -inset-1 text-[33px] opacity-70"
-                          />
-                        )}
-                      </div>
-                      <span>{item.label}</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="container py-8 bg-white rounded-[20px] shadow-custom-card mb-16">
+        <VenueInfo
+          venue={venue}
+          city={city}
+          country={country}
+          imageUrl={imageUrl}
+          imageAlt={imageAlt}
+          metaInfo={metaInfo}
+        />
         <h4 className="text-center mt-10 text-[20px] uppercase font-medium">Book {name}</h4>
         <div className="flex justify-center">
           <BookingCalendar />
@@ -102,7 +54,6 @@ const VenuePage = () => {
           />
           <span className="ml-3 text-xl">{owner?.name || "Host Name"} is the host</span>
         </div>
-
         <img src={plane} alt="plane icon flying to destination" className="mx-auto w-[300px] my-6" />
       </div>
     </div>
