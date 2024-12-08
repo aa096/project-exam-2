72f0a049dca77092e601b65e47860abc8e111132
aa096 <126397865+aa096@users.noreply.github.com>
2024-12-08 09:41:01 +0100

    banner-alt and avatar-alt fields added

--- src/components/templates/UpdateProfileTemplate.jsx
+++ src/components/templates/UpdateProfileTemplate.jsx
@@ -68,58 +68,106 @@ const UpdateProfile = () => {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-[22px] uppercase text-center mb-6">Update Your Profile</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto">
-        <div className="mb-4">
+      <h1 className="text-[22px] uppercase text-center mb-6">Edit Profile</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto ">
+        <div className="mb-6">
+          <label htmlFor="bio" className="font-medium">
+            Bio
+          </label>
           <textarea
             id="bio"
             {...register("bio")}
-            placeholder="Update your bio"
-            className="w-full border border-primary rounded-lg px-3 py-2 placeholder-primary focus:border-tertiary focus:outline-none"
+            className="w-full border border-primary rounded-lg px-3 py-2 focus:border-tertiary focus:outline-none"
           />
           {errors.bio && <p className="text-[#F3676A] text-sm mt-1">{errors.bio.message}</p>}
         </div>
 
-        <div className="mb-4">
+        <div className="mb-1">
+          <label htmlFor="avatarUrl" className="font-medium">
+            Avatar Image URL
+          </label>
           <input
             type="text"
             id="avatarUrl"
             {...register("avatar.url")}
-            placeholder="Avatar Image URL"
-            className="w-full border border-primary rounded-lg px-3 py-2 placeholder-primary focus:border-tertiary focus:outline-none"
+            className="w-full border border-primary rounded-lg px-3 py-2 focus:border-tertiary focus:outline-none"
           />
           {errors.avatar?.url && <p className="text-[#F3676A] text-sm">{errors.avatar?.url.message}</p>}
         </div>
 
-        <div className="mb-4">
+        <div className="mb-6">
+          <label htmlFor="avatarAlt" className="font-medium">
+            Alt Text
+          </label>
+          <input
+            type="text"
+            id="avatarAlt"
+            {...register("avatar.alt")}
+            className="w-full border border-primary rounded-lg px-3 py-2 focus:border-tertiary focus:outline-none"
+          />
+          {errors.avatar?.alt && <p className="text-[#F3676A] text-sm">{errors.avatar?.alt.message}</p>}
+        </div>
+
+        <div className="mb-1">
+          <label htmlFor="bannerUrl" className="font-medium">
+            Banner Image URL
+          </label>
           <input
             type="text"
             id="bannerUrl"
             {...register("banner.url")}
-            placeholder="Banner Image URL"
-            className="w-full border border-primary rounded-lg px-3 py-2 placeholder-primary focus:border-tertiary focus:outline-none"
+            className="w-full border border-primary rounded-lg px-3 py-2 focus:border-tertiary focus:outline-none"
           />
           {errors.banner?.url && <p className="text-[#F3676A] text-sm">{errors.banner?.url.message}</p>}
         </div>
 
-        <div className="mb-4">
-          <label htmlFor="venueManager" className="font-medium">
-            Are you a venue manager?
+        <div className="mb-8">
+          <label htmlFor="bannerAlt" className="font-medium">
+            Alt Text
           </label>
           <input
-            type="checkbox"
-            id="venueManager"
-            {...register("venueManager")}
-            className="accent-primary focus:ring-tertiary"
+            type="text"
+            id="bannerAlt"
+            {...register("banner.alt")}
+            className="w-full border border-primary rounded-lg px-3 py-2 focus:border-tertiary focus:outline-none"
           />
-          {errors.venueManager && <p className="text-[#F3676A] text-sm">{errors.venueManager.message}</p>}
+          {errors.banner?.alt && <p className="text-[#F3676A] text-sm">{errors.banner?.alt.message}</p>}
         </div>
 
-        <div className="flex">
+        <h2 className="uppercase font-medium my-2 text-l">What kind of Holidazer are you? *</h2>
+        <div className="flex items-center space-x-4">
+          <div>
+            <input
+              type="radio"
+              id="lookingToRent"
+              value={true}
+              {...register("venueManager")}
+              className="mr-2 accent-primary focus:ring-tertiary"
+            />
+            <label htmlFor="lookingToRent" className="font-medium">
+              I&apos;m Looking to Rent Out
+            </label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="lookingToRentOut"
+              value={false}
+              {...register("venueManager")}
+              className="mr-2 accent-primary focus:ring-tertiary"
+            />
+            <label htmlFor="lookingToRentOut" className="font-medium">
+              I&apos;m Looking to Rent
+            </label>
+          </div>
+        </div>
+        {errors.venueManager && <p className="text-[#F3676A] text-sm">{errors.venueManager.message}</p>}
+
+        <div className="flex mt-6">
           <button
             type="submit"
             className="bg-primary mx-auto uppercase text-white px-4 py-1 rounded-full hover:bg-tertiary hover:text-primary transition">
-            Update Profile
+            Edit
           </button>
         </div>
       </form>
